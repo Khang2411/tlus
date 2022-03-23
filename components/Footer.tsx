2bccce91ae14f738e038715b8d76c6c346e25bbb
Khang2411 <hk24112000@gmail.com>
2022-03-23 20:37:46 +0700

    second

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 export default function Footer() {
     return (
         <>
-            <footer className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 " style={{ backgroundColor: "#B0C4DE" }}>
+            <footer className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 position-fixed " style={{ backgroundColor: "#B0C4DE" }}>
                 <div className="text-dark mb-3 mb-md-0">
                     Đại Học Thủy Lợi
                 </div>
