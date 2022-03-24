cfe982eeeb6b401f0a01c98fd1369ed2e188ecdb
Khang2411 <hk24112000@gmail.com>
2022-03-25 05:27:23 +0700

    change123

--- pages/api/callAPI.ts
+++ pages/api/callAPI.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function callAPI(method: any, endpoint: any, dataSend: any, header: any) {
     return axios({
         method: method,
-        url: `https://admintlus-2.herokuapp.com/api/${endpoint}`,
+        url: `http://admintlus-2.herokuapp.com/api/${endpoint}`,
         data: dataSend,
         headers: { 'Content-Type': 'application/json', 'Authorization': header },
     })
