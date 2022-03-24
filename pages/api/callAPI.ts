47df82b19ed0a499045cf502ec2f37c414dc4379
Khang2411 <hk24112000@gmail.com>
2022-03-25 00:26:22 +0700

    complete-2

--- pages/api/callAPI.ts
+++ pages/api/callAPI.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function callAPI(method: any, endpoint: any, dataSend: any, header: any) {
     return axios({
         method: method,
-        url: `http://localhost:8080/admintlus/api/${endpoint}`,
+        url: `http://localhost/api/${endpoint}`,
         data: dataSend,
         headers: { 'Content-Type': 'application/json', 'Authorization': header },
     })
